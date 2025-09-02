fdb3294efec98da45736d44a8c6bfe47c0d8b076
Leonardo Bringas <leonardoisaiasbringas34@gmail.com>
2025-09-02 15:04:51 -0300

    Improves CV download and adds redirect
    Enhances the CV download functionality by using JavaScript to trigger the download and redirect the user to a thank you page.
    This provides a better user experience by giving immediate feedback after the download action.
    
    Also, opens project links in a new tab for better navigation.

--- javaScript/main.js
+++ javaScript/main.js
@@ -11,3 +11,28 @@ hamburgerBtn.addEventListener('click', () => {
 navMenu.addEventListener('click', () => {
     navMenu.classList.remove('active');
 });
+
+
+// Selecciona el botón de descarga por su ID
+const descargarCvBtn = document.getElementById('descargar-cv');
+
+// Agrega un "escuchador de eventos" para el clic
+descargarCvBtn.addEventListener('click', (event) => {
+    // Evita que el enlace # se ejecute por defecto
+    event.preventDefault();
+
+    // Inicia la descarga del archivo PDF
+    // Es importante que la ruta sea correcta
+    const link = document.createElement('a');
+    link.href = 'pdf/CV_de_LEONARDO_ISAIAS_BRINGAS_MONTERO.pdf';
+    link.download = 'CV_de_LEONARDO_ISAIAS_BRINGAS_MONTERO.pdf'; // El nombre del archivo a descargar
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+
+    // Redirige a la página de agradecimiento después de un breve retraso
+    // El "setTimeout" es para darle tiempo al navegador de iniciar la descarga
+    setTimeout(() => {
+        window.location.href = 'gracias.html';
+    }, 500); // 500 milisegundos (medio segundo)
+});
