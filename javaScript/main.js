e838f58604f1eff63092853b0f59996f677e4227
Leonardo Bringas <leonardoisaiasbringas34@gmail.com>
2025-09-03 11:26:40 -0300

    Implements hamburger menu with CSS animation
    Replaces the text-based hamburger menu icon with a CSS-animated version for improved visual appeal.
    
    The menu icon now consists of stacked lines that rotate to form an "X" when the menu is active, providing a more intuitive user experience. Also adds functionality to reset the hamburger icon state when a navigation link is clicked.

--- javaScript/main.js
+++ javaScript/main.js
@@ -7,9 +7,12 @@ hamburgerBtn.addEventListener('click', () => {
     const expanded = hamburgerBtn.getAttribute("aria-expanded") === "true" || false;
     hamburgerBtn.setAttribute("aria-expanded", !expanded);
     navMenu.classList.toggle('active');
+    hamburgerBtn.classList.toggle('active'); // Agrega o quita la clase 'active' del botón
 });
 
 // Evento para ocultar el menú al hacer clic en un enlace
 navMenu.addEventListener('click', () => {
     navMenu.classList.remove('active');
+    hamburgerBtn.classList.remove('active'); // Restablece el ícono del botón
+    hamburgerBtn.setAttribute("aria-expanded", "false"); // Restablece el atributo de accesibilidad
 });
