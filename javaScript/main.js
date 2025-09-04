33c92acfee4c2a5bae67293fb647c42c3fa18c2f
Leonardo Bringas <leonardoisaiasbringas34@gmail.com>
2025-09-04 15:09:43 -0300

    Improves mobile navigation and touch response
    Enhances the mobile navigation experience by:
    
    - Improving touch responsiveness of the logo.
    - Making the navigation menu slide in smoothly and overlay the content.
    - Ensuring the menu closes when a link is clicked.
    
    This provides a more intuitive and user-friendly navigation on smaller screens.

--- javaScript/main.js
+++ javaScript/main.js
@@ -4,15 +4,19 @@ const navMenu = document.querySelector('.nav-menu');
 
 // Evento para mostrar/ocultar el menú
 hamburgerBtn.addEventListener('click', () => {
+    console.log('Hamburguesa clickeada'); // Para depuración
     const expanded = hamburgerBtn.getAttribute("aria-expanded") === "true" || false;
     hamburgerBtn.setAttribute("aria-expanded", !expanded);
     navMenu.classList.toggle('active');
-    hamburgerBtn.classList.toggle('active'); // Agrega o quita la clase 'active' del botón
+    hamburgerBtn.classList.toggle('active');
 });
 
-// Evento para ocultar el menú al hacer clic en un enlace
-navMenu.addEventListener('click', () => {
-    navMenu.classList.remove('active');
-    hamburgerBtn.classList.remove('active'); // Restablece el ícono del botón
-    hamburgerBtn.setAttribute("aria-expanded", "false"); // Restablece el atributo de accesibilidad
+// Evento para ocultar el menú solo al hacer clic en los enlaces
+const navLinks = document.querySelectorAll('.nav-menu a');
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        navMenu.classList.remove('active');
+        hamburgerBtn.classList.remove('active');
+        hamburgerBtn.setAttribute("aria-expanded", "false");
+    });
 });
